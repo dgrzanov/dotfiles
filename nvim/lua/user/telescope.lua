066fe1a5a943c57bac76924f970c888eab3a73d4
Dino Gržanov <dino.grzanov@gmail.com>
2023-11-28 20:17:59 +0100

    * telescope open in new tab on default

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -3,6 +3,18 @@ if not status_ok then
   return
 end
 
+local actions = require("telescope.actions")
+
+telescope.setup {
+	defaults = {
+		mappings = {
+			i = {
+				["<CR>"] = actions.select_tab,
+			}
+		}
+	}
+}
+
 local builtin = require("telescope.builtin")
 
 -- KEYBINDINGS
