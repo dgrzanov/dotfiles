52308b4bcd349c2575c332bfcfb10e495a38a4d3
Dino Gržanov <dino.grzanov@gmail.com>
2025-10-03 10:42:49 +0200

    + lazyvim initial commit

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,12 +1,2 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.cmp"
-require "user.lsp"
-require "user.telescope"
-require "user.treesitter"
-require "user.autopairs"
-require "user.hardline"
-require "user.gitsigns"
-
-vim.cmd([[colorscheme everforest]])
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
